89e4976f3bfa9eef95ea180787f36ed90890e83a
TesyarRAz <mohamadtesyar@gmail.com>
2022-12-17 21:44:00 +0700

    asd

--- baju-kita-mobile/lib/repository/api.dart
+++ baju-kita-mobile/lib/repository/api.dart
@@ -3,7 +3,8 @@ import 'package:dio/dio.dart';
 class Api {
   Api._();
 
-  static const base = "http://192.168.118.1:8000";
+  // static const base = "http://192.168.118.1:8000";
+  static const base = "http://117.102.69.173:8080";
   static const api = "$base/api";
 
   static final dio = Dio(BaseOptions(
